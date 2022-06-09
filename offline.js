269fe9dee9796c662c6143a6225565b69cebea12
simulatorgonok <107195354+simulatorgonok@users.noreply.github.com>
2022-06-09 19:09:21 +0300

    Add files via upload
    New Version 1.3

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1654790819,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/tiledbackground.png",
+		"images/sprite2-sheet0.png",
+		"images/Спрайт-sheet0.png",
+		"images/ПлиточныйФон.png",
+		"media/music.ogg",
+		"icon-256.png",
+		"icon-128.png",
+		"icon-114.png",
+		"icon-32.png",
+		"icon-16.png",
+		"loading-logo.png"
+	]
+}
